370afa241b1dba8c73031032d4364f4ea22404f5
baizhuangzhuang <baizhuangzhuang@bailun.com>
2021-12-15 10:53:57 +0800

    go update

---
+++ main/Condition.go
@@ -0,0 +1,11 @@
+package main
+
+/**
+1. if
+2. if - else
+3. if 切套
+4. switch
+5. select
+	-类似 switch ,select 会随机执行一个可运行的 case .如果没有 case 可执行，将进行阻塞直到 case 可运行
+6. go 没有三目运算符
+*/
---
+++ main/Cycle.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for true {
+		fmt.Println("for 循环")
+	}
+}
+
+/**
+1. for 循环
+2. 循环嵌套
+3.循环控制语句
+	- break
+	- continue
+	- goto 将循环转移到被标记的语句
+*/
