aacd329a838109a3b53a0c842b6ccef902e27592
byebai95 <50128162+byebai95@users.noreply.github.com>
2022-03-03 14:36:29 +0800

    add
    新增文件

---
+++ main/x10_Iota.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+const(
+	a =  (1 << iota) / 4
+	b
+	c
+	d
+	e
+	f
+)
+
+func main(){
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+}
